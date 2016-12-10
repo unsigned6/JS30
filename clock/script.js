c9745ceaad7cdbef578ef60acbaaae44095e41a5
unsigned6 <yuriivlasiuk@gmail.com>
2016-12-10 17:13:55 +0200

    R^C^Cn cubic bezier.

--- clock/script.js
+++ clock/script.js
@@ -5,17 +5,11 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now  = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = (((seconds / 60) * 360) + 90)%360;
-    if (secondsDegrees === 0) {
-        secondHand.style.transition = 'none';
-        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    }
-    secondHand.style.transition = '.1s';
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(secondsDegrees);
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + 90;
+    const minsDegrees = (((mins / 60) * 360) + 90)%360;
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hours = now.getHours();
